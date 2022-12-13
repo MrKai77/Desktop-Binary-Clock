15e17a4351a89de865fcde8dd5a65f05101d63e6
Kai Azim <shotasushi77@gmail.com>
2022-12-13 07:12:42 -0700

    Initial Commit

---
+++ Binary Clock/Binary_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Binary_ClockApp.swift
+//  Binary Clock
+//
+//  Created by Kai Azim on 2022-12-13.
+//
+
+import SwiftUI
+
+@main
+struct Binary_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Binary Clock/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Binary Clock
+//
+//  Created by Kai Azim on 2022-12-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
